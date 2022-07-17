9bb865657327ffc2d632c9c797d3b48d15c5e03e
zuber57 <zubarahmad48@gmail.com>
2022-07-17 09:52:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function bulb_on(){
+    document.getElementById('bulb').src='img/HaPnm6P7TZhPQGGUxtDjAg-1200-80on.jpg';
+}
+function bulb_off(){
+    document.getElementById('bulb').src="img/light-bulb-1-1427502off.jpg";
+}
